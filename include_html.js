fae6d89b8754fdc0ebf8b412cda29c6d105e35d4
Shane McDonough <shanemcdman@gmail.com>
2021-04-07 14:49:57 -0700

    Initial

---
+++ include_html.js
@@ -0,0 +1,21 @@
+function include_html(){
+    let elements = document.getElementsByTagName('*');
+    for(let i = 0; i < elements.length; i++){
+        const element = elements[i];
+        const file_name = element.getAttribute('include-html');
+        if(file_name){
+            console.log(file_name);
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if(this.status == 200)
+                        element.insertAdjacentHTML('afterend', this.responseText);
+                    element.remove();
+                }
+            }
+            xhttp.open("GET", file_name, true);
+            xhttp.send();
+        }
+    }
+}
+include_html();
